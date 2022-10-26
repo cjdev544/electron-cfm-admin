6cb4af41cce190162154880dcdecca8df70787d3
Jefferson Campos <cjdev544@gmail.com>
2022-10-25 20:25:19 -0400

    change in homa page

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+import { format } from 'date-fns'
+
+import useOrders from '../../hooks/useOrders'
+import OrderList from '../OrdersList'
+import style from './Home.module.css'
+
 export default function HomePage() {
+  const [allOrdersToday, setAllOrdersToday] = useState(null)
+  const { orders } = useOrders()
+
+  useEffect(() => {
+    const ordersToDay = orders?.filter(
+      (order) =>
+        format(order.createdAt, 'dd/MM/yy') === format(Date.now(), 'dd/MM/yy')
+    )
+    setAllOrdersToday(ordersToDay)
+  }, [orders])
+
   return (
-    <div>
-      <h1>Pagina HomePage</h1>
+    <div className={style.home}>
+      <h1>
+        Pedidos del día <span>{allOrdersToday?.length}</span>
+      </h1>
+      <OrderList orders={allOrdersToday} />
     </div>
   )
 }

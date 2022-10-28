6f5a7a5a0ce72cd906e3f7f1bd3a02f942d702f1
Jefferson Campos <cjdev544@gmail.com>
2022-10-28 13:28:17 -0400

    get clients for buy and amount ligical ready

--- src/components/StatisticsClients/index.js
+++ src/components/StatisticsClients/index.js
@@ -1,4 +1,35 @@
+import { useEffect } from 'react'
+import { round } from 'mathjs'
+
+import useOrders from '../../hooks/useOrders'
+
 export default function StatisticsClients() {
+  const { allOrders } = useOrders()
+
+  useEffect(() => {
+    let buyUser = []
+    allOrders.forEach((order) => {
+      const clientId = order.usuario
+
+      if (clientId in buyUser) {
+        buyUser[clientId].buy += 1
+        buyUser[clientId].totalAmount += round(order.totalCompra, 2)
+        buyUser[clientId].orders = [
+          ...buyUser[clientId].orders,
+          { createdAt: order.createdAt, pedido: order.pedido },
+        ]
+      } else {
+        buyUser[clientId] = {
+          buy: 1,
+          totalAmount: order.totalCompra,
+          orders: [{ createdAt: order.createdAt, orders: order.pedido }],
+          username: order.username,
+        }
+      }
+    })
+    console.log(buyUser)
+  }, [allOrders])
+
   return (
     <div>
       <h1>StatisticsClients</h1>
